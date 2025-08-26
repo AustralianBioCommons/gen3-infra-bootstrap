390d256074c431ebab5450f814c68e7b8a89f012
Guerdon Mukama <gmukama@gmail.com>
2025-08-26 17:17:26 +1000

    Initial commit

---
+++ lib/gen3-infra-bootstrap-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Gen3InfraBootstrapStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Gen3InfraBootstrapQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
